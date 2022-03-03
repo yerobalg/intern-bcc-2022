d9da56a34f85ce72a2b143a11709087e2107dd42
yerobalg <74844505+yerobalg@users.noreply.github.com>
2022-03-03 19:31:42 +0700

    created some service for CRUD operation in produk

--- produk/service.go
+++ produk/service.go
@@ -7,3 +7,23 @@ type ProdukService struct {
 func NewProdukService(produkRepo ProdukRepository) ProdukService {
 	return ProdukService{repo: produkRepo}
 }
+
+func (s *ProdukService) Save(produk *Produk) error {
+	return s.repo.Save(produk)
+}
+
+func (s *ProdukService) GetById(id uint64) (*Produk, error) {
+	return s.repo.GetById(id)
+}
+
+func (s *ProdukService) GetByIdSeller(idSeller uint64) (*[]Produk, error) {
+	return s.repo.GetByIdSeller(idSeller)
+}
+
+func (s *ProdukService) Update(produk *Produk) error {
+	return s.repo.Update(produk)
+}
+
+func (s *ProdukService) Delete(produk *Produk) error {
+	return s.repo.Delete(produk)
+}
