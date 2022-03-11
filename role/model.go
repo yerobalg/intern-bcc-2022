e4cdd91747acb35d65fea710344a9844231bc172
yerobalg <74844505+yerobalg@users.noreply.github.com>
2022-03-11 12:02:23 +0700

    added auto increment for role model

--- role/model.go
+++ role/model.go
@@ -1,6 +1,6 @@
 package role
 
 type Roles struct {
-	ID 	 uint64   `json:"id" gorm:"primary_key;auto_increment:true"`
+	ID   uint64 `json:"id" gorm:"primary_key;auto_increment:true"`
 	Nama string `json:"name" gorm:"type:varchar(100);not null"`
 }
